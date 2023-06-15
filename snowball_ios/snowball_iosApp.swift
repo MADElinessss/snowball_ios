8eeaee8e43c8d610f05dcda3216e9d630a07379f
MADElinessss <jyseen@naver.com>
2023-06-15 15:26:40 +0900

    Initial Commit

---
+++ snowball_ios/snowball_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  snowball_iosApp.swift
+//  snowball_ios
+//
+//  Created by 신정연 on 2023/06/15.
+//
+
+import SwiftUI
+
+@main
+struct snowball_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
